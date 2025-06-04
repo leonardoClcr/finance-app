9e8bb45da8b3d1d94636c76f3c382a4ed7e65816
Leonardo <ep.leonardorocha@gmail.com>
2025-06-04 19:49:16 -0300

    feat: add GetTransactionsByUserIdUseCase

---
+++ src/use-cases/get-transactions-by-user-id.js
@@ -0,0 +1,29 @@
+import { UserNotFoundError } from "../errors/user.js";
+
+export class GetTransactionsByUserIdUseCase {
+    constructor(
+        postgresGetUserByIdRepository,
+        postgresGetTransactionsByUserIdRepository,
+    ) {
+        (this.postgresGetUserByIdRepository = postgresGetUserByIdRepository),
+            (this.postgresGetTransactionsByUserIdRepository =
+                postgresGetTransactionsByUserIdRepository);
+    }
+    async execute(params) {
+        //validar se o usuario existe
+        const user = await this.postgresGetUserByIdRepository.execute(
+            params.userId,
+        );
+
+        if (!user) {
+            return UserNotFoundError();
+        }
+        //chamar o repository
+        const transactions =
+            await this.postgresGetTransactionsByUserIdRepository.execute(
+                params.userId,
+            );
+
+        return transactions;
+    }
+}
