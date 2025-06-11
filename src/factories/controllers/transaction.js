0e3f21a4434567922e078830c59b82c1e1ed093a
Leonardo <ep.leonardorocha@gmail.com>
2025-06-11 20:00:40 -0300

    feat: add route to update a transaction

--- src/factories/controllers/transaction.js
+++ src/factories/controllers/transaction.js
@@ -5,7 +5,8 @@ import { CreateTransactionController } from "../../controllers/transaction/creat
 import { PostgresGetTransactionsByUserIdRepository } from "../../repositories/postgres/transaction/get-transactions-by-user-id.js";
 import { GetTransactionsByUserIdUseCase } from "../../use-cases/transaction/get-transactions-by-user-id.js";
 import { GetTransactionsByUserIdController } from "../../controllers/transaction/get-transactions-by-user-id.js";
-
+import { UpdateTransactionUseCase } from "../../use-cases/transaction/update-transaction.js";
+import { UpdateTransactionController } from "../../controllers/transaction/update-transaction.js";
 export const makeCreateTransactionController = () => {
     const postgresCreateTransactionRepository =
         new PostgresCreateTransactionRepository();
@@ -38,4 +39,17 @@ export const makeGetTransactionsByUserIdController = () => {
         new GetTransactionsByUserIdController(getTransactionsByUserIdUseCase);
 
     return getTransactionsByUserIdController;
+};
+
+export const makeUpdateTransactionController = () => {
+    const postgresUpdateTransactionRepository =
+        new postgresUpdateTransactionRepository();
+    const updateTransactionUseCase = new UpdateTransactionUseCase(
+        postgresUpdateTransactionRepository,
+    );
+    const updateTransactionController = new UpdateTransactionController(
+        updateTransactionUseCase,
+    );
+
+    return updateTransactionController;
 };
